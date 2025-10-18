b871c931db7e581e1a00d83dd8c74b6ac1b7cb78
umairansari92 <umair.ansari.92@gmail.com>
2025-10-19 03:41:07 +0500

    fix: optimized vite config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    // ❌ Disable lightningcss to prevent Netlify build crash
-    lightningcss: false,
-  },
-  build: {
-    outDir: "dist",
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
-          react: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+  plugins: [react() , tailwindcss()],
+})
